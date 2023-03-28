851af598f8ad2f3100154468b5172d75b73d70c7
jhlee9462 <jhlee9462@gmail.com>
2023-03-28 12:19:11 +0900

    점프모션 추가완료

--- js/jump.js
+++ js/jump.js
@@ -6,7 +6,10 @@ function jumpCharacter({style, offsetTop}) {
     /* 땅 위가 아니면 리턴 */
     if (!canGoUp(offsetTop)) return;
 
+    style.backgroundImage = 'url(../img/icon/bobby02.png)';
     style.top = (offsetTop - 140) + 'px';
+
+    jumpEnd(style);
 }
 
 function canGoDown(offsetTop) {
@@ -16,7 +19,14 @@ function canGoDown(offsetTop) {
 function downJumpCharacter({style, offsetTop}) {
     if (!canGoDown(offsetTop)) return;
 
+    style.backgroundImage = 'url(../img/icon/bobby02.png)';
     style.top = (offsetTop + 140) + 'px';
+
+    jumpEnd(style);
+}
+
+function jumpEnd(style) {
+    setTimeout(() => style.backgroundImage = 'url(../img/icon/bobby01.png)', 500);
 }
 
 export {jumpCharacter, downJumpCharacter};
