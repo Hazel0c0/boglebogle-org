9b8bf577a6c6cdfc97e1e4dd12d5073009b21ba8
jhlee9462 <jhlee9462@gmail.com>
2023-03-31 18:00:19 +0900

    중간저장

--- js/file.js
+++ js/file.js
@@ -1,9 +1,9 @@
 (() => {
-document.querySelector(".here").addEventListener('click',()=>{
-  let selectFile = document.querySelector("#chooseFile").files[0];
-  console.log(selectFile);
-const file = URL.createObjectURL(selectFile);
-document.querySelector(".monsters").style.background=`url(${file}) `;
-console.log("성공");
-})
+  document.querySelector(".here").addEventListener('click', () => {
+    let selectFile = document.querySelector("#chooseFile").files[0];
+    console.log(selectFile);
+    const file = URL.createObjectURL(selectFile);
+    document.querySelector(".monsters").style.background = `url(${file}) `;
+    console.log("성공");
+  })
 });
