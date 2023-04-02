5afa6a9ded722520b60a694189f2f356840a695d
jhlee9462 <jhlee9462@gmail.com>
2023-04-02 13:11:42 +0900

    충돌 매커니즘 수정 및 방울 갯수 제한

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,59 +1,6 @@
-import moveCharacter from "./move.js";
-import {
-
-    jumpCharacter,
-    downJumpCharacter
-} from "./jump.js";
-import attack from "./attack.js";
-import {
-    getDifficulty,
-} from "./getParameter.js";
-import makeMonsters from "./makeMonster.js";
-import moveMonster from "./monsterMove.js";
+import selectLevel from "./selectLevel.js";
 
 // main 메서드
 (() => {
-
-    let difficulty;
-
-    switch (getDifficulty()) {
-        case "상":
-            difficulty = 3;
-            break;
-        case "하":
-            difficulty = 1;
-            break;
-        default:
-            difficulty = 2;
-    }
-
-    makeMonsters(difficulty);
-
-    moveMonster(difficulty);
-
-    const $bobby = document.getElementById('bobby');
-
-    document.addEventListener('keydown', e => {
-        const keyName = e.key;
-
-        if (keyName === 'ArrowRight' || keyName === 'ArrowLeft')
-            moveCharacter($bobby, keyName);
-
-        else if (keyName === 'ArrowUp')
-            jumpCharacter($bobby);
-
-        else if (keyName === 'ArrowDown')
-            downJumpCharacter($bobby);
-
-        else if (keyName === ' ')
-            attack($bobby);
-    });
-
-
-    setInterval(() => {
-        const $monsters = document.querySelectorAll('.monster');
-
-        if (!$monsters.length)
-            makeMonsters(difficulty);
-    }, 160);
+    selectLevel();
 })();
--- js/selectLevel.js
+++ js/selectLevel.js
@@ -1,8 +1,22 @@
-import { getUserId } from "./getParameter.js";
+import {
+    getUserId
+} from "./getParameter.js";
 
-(() => {
+import gamestart from "./gamestart.js";
+
+export default function selectLevel() {
     const userId = getUserId();
 
     document.getElementById('userIdShow').textContent = userId;
     document.getElementById('userId').value = userId;
-})();
+
+    const levelBtns = [...document.querySelectorAll('.level')];
+
+    for (let i = 0; i < 3; i++) {
+        levelBtns[i].onclick = e => {
+            document.querySelector('.wrap').style.display = 'block';
+            document.querySelector('.selectLevel').style.display = 'none';
+            gamestart(3 - i);
+        }
+    }
+}
